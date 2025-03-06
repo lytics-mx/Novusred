ab43d266c9f3714ac9ce1e616056e76f573bbf38
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-03-05 23:47:24 -0600

    q

--- portal_web_lytics/controllers/main.py
+++ portal_web_lytics/controllers/main.py
@@ -1,7 +1,20 @@
+import requests
 from odoo import http
 from odoo.http import request
 
 class MiSitioWeb(http.Controller):
-    @http.route('/lytics', type='http', auth='public', website=True)
+    @http.route('/home', type='http', auth='public', website=True)
     def mi_pagina(self, **kw):
-        return request.render('portal_web_lytics.lytics_template', {})
+        # Hacer una solicitud HTTP al otro sitio web
+        response = requests.get('http://novured-desarrollo.lytics.mx:8070/es')
+        
+        # Verificar si la solicitud fue exitosa
+        if response.status_code == 200:
+            contenido = response.text
+        else:
+            contenido = "No se pudo obtener la información del sitio web."
+
+        # Renderizar la plantilla con el contenido obtenido
+        return request.render('portal_web_lytics.lytics_template', {
+            'contenido': contenido
+        })
