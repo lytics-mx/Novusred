15f87f376c2ace4f4695ff31d744e68a532bd2b2
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-03-06 00:25:58 -0600

    Update main.py

--- portal_web_lytics/controllers/main.py
+++ portal_web_lytics/controllers/main.py
@@ -2,6 +2,6 @@ from odoo import http
 from odoo.http import request
 
 class MiSitioWeb(http.Controller):
-    @http.route('/lytics', type='http', auth='public', website=True)
+    @http.route('/es', type='http', auth='public', website=True)
     def mi_pagina(self, **kw):
         return request.render('portal_web_lytics.lytics_template', {})
