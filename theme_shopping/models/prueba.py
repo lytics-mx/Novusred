9b04c912b9c8112af19918d5bcbce3bc824bbd84
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-03-08 10:33:33 -0600

    q

--- theme_shopping/models/prueba.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models, api, tools, _
-class Prueba(models.Model):
-    _name = 'prueba'
-    _description = 'prueba'
-
-    name = fields.Char(string='Name', required=True)
