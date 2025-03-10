e251b55337e7d2950cc913efbaaa703b3eb33a5b
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-03-10 17:06:17 -0600

    q

--- theme_xtream/controllers/theme_xtream.py
+++ theme_xtream/controllers/theme_xtream.py
@@ -73,6 +73,7 @@ class WebsiteProduct(http.Controller):
         })
         return True
     
+
     @http.route('/about', auth='public', website=True)
     def about(self, **kw):
         return http.request.render('theme_xtream.xtream_about')
