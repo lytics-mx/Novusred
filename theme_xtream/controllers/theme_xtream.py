5390f1ccc8703e02bf7a10d817765a6563d773f2
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-03-21 17:53:01 -0600

    q

--- theme_xtream/controllers/theme_xtream.py
+++ theme_xtream/controllers/theme_xtream.py
@@ -114,11 +114,11 @@ class WebsiteProduct(http.Controller):
         """
         return http.request.render('theme_xtream.descubre')
 
-    @http.route('/', auth='public', website=True)
+    @http.route('/home', auth='public', website=True)
     def home(self, **kw):
         """
         Renderiza la página de inicio.
         """
-        return http.request.render('website.homepage')
+        return http.request.render('theme_xtream.xtream_inicio')
 
 
