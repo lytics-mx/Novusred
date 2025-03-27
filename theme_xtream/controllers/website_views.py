f2f8ea17bc1e645c0ec655d9ebda1e5a6440ed90
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-03-27 01:18:10 -0600

    q

--- theme_xtream/controllers/website_views.py
+++ theme_xtream/controllers/website_views.py
@@ -10,10 +10,12 @@ class WebsiteShop(http.Controller):
     def lg_products(self, **kw):
         return http.request.render('theme_xtream.lg_products_page')
 
+    @http.route('/epson', auth='public', website=True)
+    def epson_products(self, **kw):
+        return http.request.render('theme_xtream.products_epson')
+
     @http.route(['/shop/visited_products'], type='http', auth="public", website=True)
     def visited_products(self, **kwargs):
         visited_product_ids = http.request.session.get('visited_product_ids', [])
         visited_products = request.env['product.template'].browse(visited_product_ids)
         return http.request.render("theme_xtream.visited_products", {'visited_products': visited_products})
-    
-
