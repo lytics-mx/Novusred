106424faec996758ce29dfa4bde947bfb6d6dc65
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-03-31 07:34:17 -0600

    q

--- theme_xtream/models/product_category.py
+++ theme_xtream/models/product_category.py
@@ -5,3 +5,19 @@ class ProductCategory(models.Model):
 
     icon = fields.Binary(string="Category Icon")  # Campo para subir el ícono
     is_visible_in_menu = fields.Boolean(string="Visible", default=False)  # Campo booleano para habilitar visibilidad
+
+
+    @classmethod
+    def get_visible_categories(cls):
+        """Obtiene las categorías principales y subcategorías visibles."""
+        categories = cls.env['product.category'].search([('is_visible_in_menu', '=', True), ('parent_id', '=', False)])
+        result = []
+        for category in categories:
+            result.append({
+                'name': category.name,
+                'subcategories': [
+                    {'name': subcategory.name}
+                    for subcategory in category.child_id.filtered(lambda c: c.is_visible_in_menu)
+                ]
+            })
+        return result
