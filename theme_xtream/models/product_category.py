52ad622738e91867df382b8a0600d8a66eefa461
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-03-31 07:06:59 -0600

    Update product_category.py

--- theme_xtream/models/product_category.py
+++ theme_xtream/models/product_category.py
@@ -4,4 +4,4 @@ class ProductCategory(models.Model):
     _inherit = 'product.category'
 
     icon = fields.Binary(string="Category Icon")  # Campo para subir el ícono
-    is_visible_in_menu = fields.Boolean(string="Visible", default=False)  # Campo booleano para habilitar visibilidad
+    is_visible_in_menu = fields.Boolean(string="Visible")  # Campo booleano para habilitar visibilidad
