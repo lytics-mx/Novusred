b21f98e70c99a54fc0f028570b5fdbcd95110d5a
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-03-31 08:27:31 -0600

    q

--- theme_xtream/controllers/historial_views.py
+++ theme_xtream/controllers/historial_views.py
@@ -1,13 +1,13 @@
-from odoo import http
-from odoo.http import request
+# from odoo import http
+# from odoo.http import request
 
-class ProductHistoryController(http.Controller):
+# class ProductHistoryController(http.Controller):
 
-    @http.route('/shop/history', type='http', auth='user', website=True)
-    def view_history(self):
-        """Obtiene el historial de productos vistos por el usuario actual."""
-        user_id = request.env.user.id
-        history = request.env['product.view.history'].sudo().search([('user_id', '=', user_id)], order='viewed_at desc')
-        return request.render('theme_xtream.history_template', {
-            'history': history,
-        })
+#     @http.route('/shop/history', type='http', auth='user', website=True)
+#     def view_history(self):
+#         """Obtiene el historial de productos vistos por el usuario actual."""
+#         user_id = request.env.user.id
+#         history = request.env['product.view.history'].sudo().search([('user_id', '=', user_id)], order='viewed_at desc')
+#         return request.render('theme_xtream.history_template', {
+#             'history': history,
+#         })
--- theme_xtream/models/historial.py
+++ theme_xtream/models/historial.py
@@ -1,18 +1,18 @@
-from odoo import models, fields, api
+# from odoo import models, fields, api
 
-class ProductViewHistory(models.Model):
-    _name = 'product.view.history'
-    _description = 'Historial de productos vistos'
+# class ProductViewHistory(models.Model):
+#     _name = 'product.view.history'
+#     _description = 'Historial de productos vistos'
 
-    user_id = fields.Many2one('res.users', string="Usuario", required=True, ondelete='cascade')
-    product_id = fields.Many2one('product.product', string="Producto", required=True, ondelete='cascade')
-    viewed_at = fields.Datetime(string="Visto en", default=fields.Datetime.now)
+#     user_id = fields.Many2one('res.users', string="Usuario", required=True, ondelete='cascade')
+#     product_id = fields.Many2one('product.product', string="Producto", required=True, ondelete='cascade')
+#     viewed_at = fields.Datetime(string="Visto en", default=fields.Datetime.now)
 
-    @api.model
-    def add_product_to_history(self, product_id):
-        """Agrega un producto al historial del usuario actual."""
-        if self.env.user.id:
-            self.create({
-                'user_id': self.env.user.id,
-                'product_id': product_id,
-            })
+#     @api.model
+#     def add_product_to_history(self, product_id):
+#         """Agrega un producto al historial del usuario actual."""
+#         if self.env.user.id:
+#             self.create({
+#                 'user_id': self.env.user.id,
+#                 'product_id': product_id,
+#             })
