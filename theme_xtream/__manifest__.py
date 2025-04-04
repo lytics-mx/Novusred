b6d335e8ff36b177c4e18f1addb068357a3f5455
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-04 12:57:40 -0600

    q

--- theme_xtream/__manifest__.py
+++ theme_xtream/__manifest__.py
@@ -68,6 +68,7 @@
         'views/marca_web.xml',
         'views/category/category.xml',
         'views/shop/history.xml',
+        'views/shop/shop_product.xml',
         # 'views/header_logo.xml',
         # 'views/shop/brand.xml',
         
