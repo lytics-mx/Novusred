b3cc9335435e7ff25612566d8c38498924b8ac3f
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-03 21:12:23 -0600

    q

--- theme_xtream/controllers/brand_product.py
+++ theme_xtream/controllers/brand_product.py
@@ -2,8 +2,6 @@ from odoo import http
 from odoo.http import request
 
 class BrandController(http.Controller):
-
-
     @http.route('/brand', auth='public', website=True)
     def brand_page(self, **kw):
         return http.request.render('theme_xtream.website_sale.product_custom')
