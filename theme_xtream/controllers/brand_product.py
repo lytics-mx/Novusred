a36619efca094eeb159c382b7bcbef7662cfaca5
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-03 21:03:40 -0600

    q

--- theme_xtream/controllers/brand_product.py
+++ theme_xtream/controllers/brand_product.py
@@ -3,32 +3,7 @@ from odoo.http import request
 
 class BrandController(http.Controller):
 
-    @http.route(['/brand'], type='http', auth="public", website=True)
-    def shop_page(self, **kwargs):
-        # Obtener todas las etiquetas disponibles (usadas como marcas)
-        brands = request.env['product.tag'].sudo().search([])  # Correct model name
 
-        # Obtener todas las categorías disponibles
-        categories = request.env['product.public.category'].sudo().search([])
-
-        # Filtrar productos según los parámetros (etiquetas y categorías)
-        domain = []
-        if 'tags' in kwargs:
-            tag_ids = [int(t) for t in kwargs.getlist('tags')]
-            domain.append(('product_tag_ids', 'in', tag_ids))
-        if 'category' in kwargs:
-            domain.append(('public_categ_ids', 'in', [int(kwargs['category'])]))
-
-        # Obtener los productos filtrados
-        products = request.env['product.template'].sudo().search(domain)
-
-        # Determinar el tipo de vista (cuadrícula o lista)
-        view_type = request.httprequest.cookies.get('view_type', 'grid')
-
-        # Renderizar la plantilla con los datos
-        return request.render('theme_xtream.product_grid_list_toggle', {
-            'brands': brands,  # Pasar las etiquetas como "marcas"
-            'categories': categories,
-            'products': products,
-            'view_type': view_type,
-        })
+    @http.route('/brand', auth='public', website=True)
+    def about(self, **kw):
+        return http.request.render('theme_xtream.website_sale.product_custom')
