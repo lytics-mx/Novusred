3b744b53ba74a94354c86b9e0f741c32a5cedcb9
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-13 23:38:27 -0600

    q

--- theme_xtream/__manifest__.py
+++ theme_xtream/__manifest__.py
@@ -73,9 +73,9 @@
 # OFERTAS        
         'views/shop/ofertas.xml',
         'views/shop/offers_views.xml',
+        'views/shop/ofertas/product_template_tree_view.xml',
         'views/shop/ofertas/action_offers.xml',
-        'views/shop/ofertas/menu_offers.xml',
-        'views/shop/ofertas/product_template_tree_view.xml',        
+        'views/shop/ofertas/menu_offers.xml',      
 
 
 
