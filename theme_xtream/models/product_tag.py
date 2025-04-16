1ca399615c517811ac04899ceaefb80bd4c0eb2f
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-15 23:06:25 -0600

    Update product_tag.py

--- theme_xtream/models/product_tag.py
+++ theme_xtream/models/product_tag.py
@@ -1,19 +1,31 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 class ProductTag(models.Model):
-    _inherit = 'product.tag'
+    _name = 'product.tag'
+    _description = 'Product Tag'
 
-    discount_percentage = fields.Float(
-        string="Descuento (%)",
-        help="Porcentaje de descuento aplicado a los productos con esta etiqueta."
+    discount_percentage = fields.Float(string="Descuento (%)", help="Porcentaje de descuento asociado a esta etiqueta.")
+    product_ids = fields.Many2many(
+        'product.template',
+        'product_template_tag_rel',  # Nombre de la tabla de relación
+        'tag_id',  # Campo de relación a la etiqueta
+        'product_id',  # Campo de relación al producto
+        string="Productos Relacionados",
+        help="Productos asociados con esta etiqueta."
     )
 
     def write(self, vals):
-        """Aplica el descuento a los productos relacionados al guardar."""
+        """Actualiza automáticamente el campo tag_ids en los productos relacionados."""
         res = super(ProductTag, self).write(vals)
-        if 'discount_percentage' in vals:
+        if 'product_ids' in vals:
             for tag in self:
-                products = self.env['product.template'].search([('tag_ids', 'in', tag.id)])
-                products._compute_discount_percentage_from_tags()
-                products._compute_discounted_price()
+                # Actualiza el campo tag_ids en los productos relacionados
+                tag.product_ids.write({'tag_ids': [(4, tag.id)]})
         return res
+
+    def create(self, vals):
+        """Asegura que los productos relacionados se actualicen al crear una etiqueta."""
+        tag = super(ProductTag, self).create(vals)
+        if 'product_ids' in vals:
+            tag.product_ids.write({'tag_ids': [(4, tag.id)]})
+        return tag
