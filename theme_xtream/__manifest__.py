ea4bca4cb6c31e372c0d0f168eee22706e2555aa
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-16 18:19:00 -0600

    Update __manifest__.py

--- theme_xtream/__manifest__.py
+++ theme_xtream/__manifest__.py
@@ -80,7 +80,7 @@
         'views/website_sale/product_template.xml',
         'views/website_sale/shop_filters.xml',
 
-        'data/ir_cron_data.xml',
+        # 'data/ir_cron_data.xml',
 
 
     ],
