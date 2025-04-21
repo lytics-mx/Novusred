c8581714c0a38ee3a80c1920ce7c2ddd99a1b885
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-20 23:22:49 -0600

    Update website_views.py

--- theme_xtream/controllers/website_views.py
+++ theme_xtream/controllers/website_views.py
@@ -1,24 +1,23 @@
 from odoo import http
 from odoo.http import request
-
 class OffersController(http.Controller):
 
     @http.route('/shop/offers', type='http', auth='public', website=True)
     def offers_page(self, brand_id=None, category_id=None, **kwargs):
         # Filtrar productos publicados con descuento
-        domain = [('website_published', '=', True), ('list_price', '>', 0)]
+        domain = [('website_published', '=', True), ('lst_price', '>', 0)]
         if brand_id:
-            domain.append(('brand_id', '=', int(brand_id)))
+            domain.append(('brand_type_id', '=', int(brand_id)))
         if category_id:
             domain.append(('categ_id', '=', int(category_id)))
 
         products = request.env['product.template'].sudo().search(domain)
 
         # Obtener marcas relacionadas con productos publicados
-        brands = request.env['product.brand'].sudo().search([])
+        brands = request.env['brand.type'].sudo().search([])
         for brand in brands:
             brand.product_count = request.env['product.template'].sudo().search_count(
-                [('brand_id', '=', brand.id), ('website_published', '=', True)]
+                [('brand_type_id', '=', brand.id), ('website_published', '=', True)]
             )
 
         # Obtener categorías relacionadas con productos publicados
