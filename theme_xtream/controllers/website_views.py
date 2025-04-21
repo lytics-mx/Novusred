715caad971e4092bc20fe787f71117817c6d9152
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-20 23:32:58 -0600

    Update website_views.py

--- theme_xtream/controllers/website_views.py
+++ theme_xtream/controllers/website_views.py
@@ -6,9 +6,12 @@ class OffersController(http.Controller):
     @http.route('/offers', type='http', auth='public', website=True)
     def offers(self, **kwargs):
         """Renderiza la página de productos en oferta."""
+        # Filtrar productos publicados con precio con descuento o relacionados con etiquetas
         discounted_products = request.env['product.template'].sudo().search([
             ('website_published', '=', True),
-            ('discount_price', '>', 0)  # Busca productos con precio de descuento mayor a 0
+            '|',  # OR lógico
+            ('discounted_price', '>', 0),  # Productos con precio con descuento
+            ('product_tag_ids', '!=', False)  # Productos relacionados con etiquetas
         ])
         return request.render('theme_xtream.offers_template', {
             'discounted_products': discounted_products
