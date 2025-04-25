8200c2febeeb9a200730fe525d1e2daced9f2627
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-25 02:54:04 -0600

    Q

--- theme_xtream/controllers/product_views.py
+++ theme_xtream/controllers/product_views.py
@@ -3,8 +3,15 @@ from odoo.http import request
 
 class ShopController(http.Controller):
 
-    @http.route(['/shop/product/<model("product.product"):product>'], type='http', auth="public", website=True)
-    def product(self, product, **kwargs):
-        # Llama al método para registrar el producto visto
-        request.env['product.view.history'].sudo().add_product_to_history(product.id)
+    @http.route(['/shop/product/<model("product.template"):product>'], type='http', auth="public", website=True)
+    def product_page(self, product, **kwargs):
+        # Registrar el producto visitado
+        if request.env.user.partner_id:
+            visitor = request.env['website.visitor']._get_visitor_from_request()
+            if visitor:
+                request.env['website.track'].sudo().create({
+                    'visitor_id': visitor.id,
+                    'product_id': product.id,
+                })
+        # Renderizar la página del producto
         return request.render("website_sale.product", {'product': product})
