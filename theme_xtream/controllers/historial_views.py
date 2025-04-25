0e8bc066802ca912b70324763bcebfa76b4067d3
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-24 19:20:17 -0600

    q

--- theme_xtream/controllers/historial_views.py
+++ theme_xtream/controllers/historial_views.py
@@ -23,7 +23,7 @@ class ProductHistoryController(http.Controller):
             'grouped_history': dict(grouped_history),
         })
 
-    @http.route('/shop/history/remove/<int:product_id>', type='json', auth='user', website=True)
+    @http.route('/shop/history/remove/<int:product_id>', type='http', auth='user', website=True)
     def remove_from_history(self, product_id):
         """Elimina un producto del historial del usuario actual."""
         user_partner_id = request.env.user.partner_id.id
@@ -32,9 +32,10 @@ class ProductHistoryController(http.Controller):
             ('visitor_id.partner_id', '=', user_partner_id),
             ('product_id.product_tmpl_id', '=', product_id)
         ], limit=1)
-    
+
         # Eliminar el registro si existe
         if track_entry:
             track_entry.unlink()
-    
-        return {'success': True}
+
+        # Redirigir de vuelta al historial
+        return request.redirect('/shop/history')
