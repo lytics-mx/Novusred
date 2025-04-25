a308279d6100989673166aaeef4a881270ff8362
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-25 03:10:13 -0600

    w

--- theme_xtream/controllers/product_views.py
+++ theme_xtream/controllers/product_views.py
@@ -5,13 +5,23 @@ class ShopController(http.Controller):
 
     @http.route(['/shop/product/<model("product.template"):product>'], type='http', auth="public", website=True)
     def product_page(self, product, **kwargs):
-        # Registrar el producto visitado
-        if request.env.user.partner_id:
-            visitor = request.env['website.visitor']._get_visitor_from_request()
-            if visitor:
+        """Registra el producto visitado y restablece el estado oculto si es necesario."""
+        visitor = request.env['website.visitor']._get_visitor_from_request()
+        if visitor:
+            track_entry = request.env['website.track'].sudo().search([
+                ('visitor_id', '=', visitor.id),
+                ('product_id', '=', product.id)
+            ], limit=1)
+    
+            if track_entry:
+                # Si ya existe, restablecer el estado oculto
+                track_entry.sudo().write({'hidden': False})
+            else:
+                # Crear un nuevo registro si no existe
                 request.env['website.track'].sudo().create({
                     'visitor_id': visitor.id,
                     'product_id': product.id,
                 })
+    
         # Renderizar la página del producto
         return request.render("website_sale.product", {'product': product})
