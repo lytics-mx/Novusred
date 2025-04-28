c70b7402df8d37be8ea13d4a8b5ce681db37ae7b
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-28 11:52:20 -0600

    q

--- theme_xtream/models/banner_website.py
+++ theme_xtream/models/banner_website.py
@@ -1,15 +1,31 @@
 from odoo import fields, models
 
-class BannerImageLine(models.Model):
-    """
-    Model for individual images
-    """
-    _name = 'banner.image.line'
-    _description = "Banner Image Line"
 
-    image_set_id = fields.Many2one(
-        'banner.images', string="Image Set",
-        required=True, ondelete='cascade'
+class BannerImages(models.Model):
+    """
+    Model for managing banner images
+    """
+    _name = 'banner.images'
+    _description = "Banner Images"
+
+    name = fields.Char(string="Name", required=True, help="Name of the image set")
+    
+    general_images = fields.Many2many(
+        'ir.attachment',
+        'banner_general_images_rel',  # Nombre de la tabla de relación
+        'banner_id',  # Campo que relaciona con este modelo
+        'attachment_id',  # Campo que relaciona con ir.attachment
+        string="General Images",
+        domain=[('mimetype', 'ilike', 'image/')],  # Solo permite imágenes
+        help="Sube imágenes generales."
     )
-    image = fields.Binary(string="Image", required=True, help="Upload the image")
-    description = fields.Char(string="Description", help="Description of the image")
+    
+    product_images = fields.Many2many(
+        'ir.attachment',
+        'banner_product_images_rel',  # Nombre de la tabla de relación
+        'banner_id',  # Campo que relaciona con este modelo
+        'attachment_id',  # Campo que relaciona con ir.attachment
+        string="Product Images",
+        domain=[('mimetype', 'ilike', 'image/')],  # Solo permite imágenes
+        help="Sube imágenes de productos."
+    )
