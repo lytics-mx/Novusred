dbf1279f436be083682eac470fee7f4df486f719
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-28 04:56:42 -0600

    q

--- theme_xtream/models/banner_website.py
+++ theme_xtream/models/banner_website.py
@@ -1,49 +1,17 @@
 from odoo import fields, models
 
 
-class BannerImages(models.Model):
+class BannerImageLine(models.Model):
     """
-    Model for managing images
+    Model for individual images
     """
-    _name = 'banner.images'
-    _description = "Xtream Images"
+    _name = 'banner.image.line'
+    _description = "Banner Image Line"
 
-    name = fields.Char(string="Name", required=True, help="Name of the image set")
-    image_quantity = fields.Selection([
-        ('2', '2 Images'),
-        ('4', '4 Images'),
-        ('8', '8 Images'),
-        ('12', '12 Images'),
-        ('14', '14 Images')
-    ], string="Number of Cover Images", default='2', required=True)
-    
-    product_image_quantity = fields.Selection([
-        ('2', '2 Images'),
-        ('4', '4 Images'),
-        ('8', '8 Images'),
-        ('12', '12 Images'),
-        ('14', '14 Images')
-    ], string="Number of Product Images", default='2', required=True)
-    
-    cover_images = fields.One2many(
-        'banner.image.line', 'image_set_id',
-        string="Cover Images",
-        help="Upload multiple cover images"
+    image_set_id = fields.Many2one(
+        'banner.images', string="Image Set",
+        required=True, ondelete='cascade'
     )
-    product_cover_images = fields.One2many(
-        'banner.image.line', 'image_set_id',
-        string="Product Cover Images",
-        help="Upload multiple product cover images"
-    )
-
-    # New fields for the Website tab
-    website_published = fields.Boolean(
-        string="Published on Website",
-        default=False,
-        help="Check this box to publish the images on the website"
-    )
-    website_description = fields.Text(
-        string="Website Description",
-        help="Description to display on the website"
-    )
+    image = fields.Binary(string="Image", required=True, help="Upload the image")
+    description = fields.Char(string="Description", help="Description of the image")
 
