4b9568baa12cb82a8e3fdaaa6417ab8383210159
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-29 04:13:09 -0600

    Update product_views.py

--- theme_xtream/controllers/product_views.py
+++ theme_xtream/controllers/product_views.py
@@ -3,7 +3,7 @@ from odoo.http import request
 
 class ShopController(http.Controller):
 
-    @http.route(['/shop/product/<model("product.product"):product>'], type='http', auth="public", website=True)
+    @http.route(['/shop/productos/<model("product.product"):product>'], type='http', auth="public", website=True)
     def product(self, product, **kwargs):
         # Llama al método para registrar el producto visto
         request.env['product.view.history'].sudo().add_product_to_history(product.id)
