29a11338e7d38a02ac0dda7c7ab427352320ccac
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-29 05:15:20 -0600

    q

--- theme_xtream/controllers/product_views.py
+++ theme_xtream/controllers/product_views.py
@@ -8,5 +8,7 @@ class ShopController(http.Controller):
 
     @http.route(['/tienda/producto/<model("product.product"):product>'], type='http', auth="public", website=True)
     def product_page(self, product, **kwargs):
+        if not product:
+            return request.not_found()  # Devuelve un error 404 si el producto no existe
         _logger.info("Producto cargado: %s", product)
-        return request.render("theme_xtream.product_detail_template", {'product': product})    
+        return request.render("theme_xtream.product_detail_template", {'product': product})
