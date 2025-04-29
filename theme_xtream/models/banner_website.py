49f6cfb9a7d384a34c0e3991fe61cb4ed86068c9
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-29 00:15:11 -0600

    q

--- theme_xtream/models/banner_website.py
+++ theme_xtream/models/banner_website.py
@@ -26,12 +26,35 @@ class BannerImageLine(models.Model):
         domain=[('mimetype', 'ilike', 'image/')],
         help="Upload product cover images."
     )
+    brand_images = fields.Many2many(
+        'ir.attachment',
+        'banner_brand_images_rel',
+        'banner_id',
+        'attachment_id',
+        string="Brand Images",
+        domain=[('mimetype', 'ilike', 'image/')],  # Only allow image files
+        help="Upload images related to brands."
+    )
 
     is_active_carousel = fields.Boolean(string="Mostrar en Carrusel General", default=False)
     is_active_product_carousel = fields.Boolean(string="Mostrar en Carrusel de Productos", default=False)
+    is_active_brand_carousel = fields.Boolean(string="Mostrar en Carrusel de Marcas", default=False)
+
 
     @api.onchange('is_active_product_carousel')
     def _onchange_is_active_product_carousel(self):
         """Asegura que solo un registro esté activo en el carrusel de productos"""
         if self.is_active_product_carousel:
             self.sudo().search([('id', '!=', self.id)]).write({'is_active_product_carousel': False})
+ 
+    @api.onchange('is_active_carousel')
+    def _onchange_is_active_carousel(self):
+        """Asegura que solo un registro esté activo en el carrusel"""
+        if self.is_active_carousel:
+            self.sudo().search([('id', '!=', self.id)]).write({'is_active_carousel': False}) 
+
+    @api.onchange('is_active_brand_carousel')
+    def _onchange_is_active_brand_carousel(self):
+        """Ensure only one record is active for the brand carousel"""
+        if self.is_active_brand_carousel:
+            self.sudo().search([('id', '!=', self.id)]).write({'is_active_brand_carousel': False})
