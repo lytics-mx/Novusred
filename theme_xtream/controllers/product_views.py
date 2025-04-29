11cb026520af08f9db57e2eedac0f402afaf4742
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-29 04:56:07 -0600

    q

--- theme_xtream/controllers/product_views.py
+++ theme_xtream/controllers/product_views.py
@@ -5,5 +5,5 @@ class ShopController(http.Controller):
 
     @http.route(['/tienda/producto/<model("product.product"):product>'], type='http', auth="public", website=True)
     def product_page(self, product, **kwargs):
-        # Renderiza la nueva plantilla personalizada con el producto seleccionado
+        # Renderiza la plantilla personalizada con el producto seleccionado
         return request.render("theme_xtream.product_detail_template", {'product': product})
