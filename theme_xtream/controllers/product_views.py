f4f59001938521bae26e20ac4817912a65c64c24
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-04-29 05:09:44 -0600

    q

--- theme_xtream/controllers/product_views.py
+++ theme_xtream/controllers/product_views.py
@@ -1,9 +1,12 @@
 from odoo import http
 from odoo.http import request
-
+import logging
+_logger = logging.getLogger(__name__)
+    
 class ShopController(http.Controller):
+
 
     @http.route(['/tienda/producto/<model("product.product"):product>'], type='http', auth="public", website=True)
     def product_page(self, product, **kwargs):
-        # Renderiza la plantilla personalizada con el producto seleccionado
-        return request.render("theme_xtream.product_detail_template", {'product': product})
+        _logger.info("Producto cargado: %s", product)
+        return request.render("theme_xtream.product_detail_template", {'product': product})    
