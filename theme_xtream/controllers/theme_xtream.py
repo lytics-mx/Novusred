375610fc21b2282b0de7410b7cc8245a7629bb26
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-05-02 00:18:49 -0600

    q

--- theme_xtream/controllers/theme_xtream.py
+++ theme_xtream/controllers/theme_xtream.py
@@ -125,10 +125,16 @@ class WebsiteProduct(http.Controller):
     @http.route('/', auth='public', website=True)
     def home(self, **kw):
         """
-        Renderiza la página de inicio.
+        Renderiza la página de inicio detectando el tipo de dispositivo.
         """
-        return http.request.render('theme_xtream.xtream_inicio')
-    
+        user_agent = request.httprequest.user_agent.string.lower()
+        is_mobile = 'mobile' in user_agent or 'android' in user_agent or 'iphone' in user_agent
+        
+        if is_mobile:
+            return http.request.render('theme_xtream.xtream_inicio_mobile')
+        else:
+            return http.request.render('theme_xtream.xtream_inicio')
+        
     @http.route('/brand', auth='public', website=True)
     def brand(self, **kw):
         """
