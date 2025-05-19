6e4ab3190402918f97fdb5d9d3119f4c3d67cd47
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-05-19 11:28:32 -0600

    q

--- theme_xtream/models/product_brand.py
+++ theme_xtream/models/product_brand.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class BrandType(models.Model):
     _name = 'brand.type'
@@ -22,3 +23,18 @@ class BrandType(models.Model):
         default=True,
         help="Desactiva esta marca para que no se muestre en el carrusel."
     )
+
+    product_ids = fields.Many2many(
+        'product.template',
+        'brand_type_product_rel',
+        'brand_type_id',
+        'product_id',
+        string="Productos Relacionados",
+        help="Selecciona hasta 3 productos relacionados a esta marca."
+    )
+
+    @api.constrains('product_ids')
+    def _check_product_ids_limit(self):
+        for record in self:
+            if len(record.product_ids) > 3:
+                raise ValidationError("Solo puedes seleccionar hasta 3 productos relacionados.")
