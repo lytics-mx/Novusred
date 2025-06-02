4ae6b454fc04ce117ea9a36d58cb94b5ba782376
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-06-02 01:54:59 -0600

    q

--- theme_xtream/controllers/brand_product.py
+++ theme_xtream/controllers/brand_product.py
@@ -3,20 +3,6 @@ from odoo.http import request
 
 class WebsiteBrand(http.Controller):
 
-    @http.route(['/brand/products'], type='json', auth="public", website=True)
-    def filter_products(self, tag_ids=None):
-        domain = [('website_published', '=', True)]
-        if tag_ids:
-            domain.append(('tag_ids', 'in', tag_ids))
-        
-        products = request.env['product.template'].sudo().search(domain)
-        products_data = []
-        for product in products:
-            products_data.append({
-                'id': product.id,
-                'name': product.name,
-                'price': request.env['ir.qweb.field.monetary'].value_to_html(
-                    product.list_price, {'display_currency': request.env.company.currency_id}),
-                'image': product.image_1920 and f"data:image/png;base64,{product.image_1920.decode('utf-8')}" or None,
-            })
-        return products_data
+    @http.route('/brand', auth='public', website=True)
+    def home(self):
+        return http.request.render('theme_xtream.website_brand')  
