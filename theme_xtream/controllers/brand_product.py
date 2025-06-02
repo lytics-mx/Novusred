b647fbcbe6cda6056fe3cf778d0923be7e50dd29
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-06-02 02:15:02 -0600

    Update brand_product.py

--- theme_xtream/controllers/brand_product.py
+++ theme_xtream/controllers/brand_product.py
@@ -5,21 +5,22 @@ class WebsiteBrand(http.Controller):
 
     @http.route('/brand/<string:brand_type>', auth='public', website=True)
     def brand_products(self, brand_type):
-        # Buscar el tipo de marca por el campo 'slug' o 'name'
         BrandType = request.env['brand.type']
         brand_type_rec = BrandType.sudo().search([('slug', '=', brand_type)], limit=1)
         if not brand_type_rec:
             return request.not_found()
 
-        # Buscar productos que coincidan con ese tipo de marca
         products = request.env['product.template'].sudo().search([('brand_type_id', '=', brand_type_rec.id)])
 
-        # Renderizar el template con los productos y el tipo de marca
         return request.render('theme_xtream.website_brand', {
             'brand_type': brand_type_rec,
             'products': products,
         })
-    
+
     @http.route('/brand', auth='public', website=True)
     def home(self):
-        return http.request.render('theme_xtream.website_brand')  
+        # Mostrar productos normales si no hay marca seleccionada
+        products = request.env['product.template'].sudo().search([('website_published', '=', True)], order='create_date desc', limit=10)
+        return http.request.render('theme_xtream.website_brand', {
+            'products': products,
+        })
