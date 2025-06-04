60654343a2bbbd627b93366a3cc5cf3786e34731
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-06-03 22:05:09 -0600

    Update brand_product.py

--- theme_xtream/controllers/brand_product.py
+++ theme_xtream/controllers/brand_product.py
@@ -13,9 +13,18 @@ class WebsiteBrand(http.Controller):
 
         products = request.env['product.template'].sudo().search([('brand_type_id', '=', brand_type_rec.id)])
 
+        # Buscar el banner alternativo solo si no hay cover_image
+        active_brand_banner = None
+        if not brand_type_rec.cover_image:
+            active_brand_banner = request.env['banner.image.line'].sudo().search([
+                ('is_active_brand_carousel', '=', True),
+                ('name', '=', 'marcas')
+            ], limit=1)
+
         return request.render('theme_xtream.brand_search', {
             'brand_type': brand_type_rec,
             'products': products,
+            'active_brand_banner': active_brand_banner,
         })
     
     @http.route('/brand_search_redirect', type='http', auth='public', website=True)
