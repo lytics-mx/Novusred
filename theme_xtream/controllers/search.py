88ed65756bdefdfa3dbf4ece933c70173e362683
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-06-05 01:52:17 -0600

    q

--- theme_xtream/controllers/search.py
+++ theme_xtream/controllers/search.py
@@ -1,26 +1,15 @@
+# No se indica filepath, colócalo en tu controlador de Odoo
 from odoo import http
 from odoo.http import request
 
-class SearchRedirectController(http.Controller):
+class WebsiteSearch(http.Controller):
 
     @http.route('/search_redirect', auth='public', website=True)
-    def search_redirect(self, search=None, **kwargs):
-        if not search:
-            return request.redirect('/')
-
-        # Buscar primero si es una marca
-        brand = request.env['brand.type'].sudo().search([
-            '|', ('name', 'ilike', search), ('slug', '=', search.lower())
-        ], limit=1)
-        if brand:
-            return request.redirect('/brand/' + (brand.slug or str(brand.id)))
-
-        # Buscar si es una categoría
-        category = request.env['product.category'].sudo().search([
-            '|', ('name', 'ilike', search), ('slug', '=', search.lower())
-        ], limit=1)
-        if category:
-            return request.redirect('/category/' + (category.slug or str(category.id)))
-
-        # Si no es marca ni categoría, puedes redirigir a búsqueda general de productos
-        return request.redirect('/shop?search=' + search)
+    def search_redirect(self, search='', search_type='all', **kw):
+        if search_type == 'brand':
+            return request.redirect('/brand_search?search=%s' % search)
+        elif search_type == 'category':
+            return request.redirect('/category_search?search=%s' % search)
+        else:
+            # Lógica para búsqueda general (puedes personalizar)
+            return request.redirect('/shop?search=%s' % search)
