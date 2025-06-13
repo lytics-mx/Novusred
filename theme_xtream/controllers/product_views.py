db1f9e517869f99e7dce7d6029336a0184f573d2
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-06-13 04:02:25 -0600

    Q

--- theme_xtream/controllers/product_views.py
+++ theme_xtream/controllers/product_views.py
@@ -57,6 +57,14 @@ class ShopController(WebsiteSale):
                 ('id', '!=', product.id),
                 ('website_published', '=', True)
             ])
+        
+        related_tag_products = []
+        if product.product_tag_ids:
+            related_tag_products = request.env['product.template'].sudo().search([
+                ('product_tag_ids', 'in', product.product_tag_ids.ids),
+                ('id', '!=', product.id),
+                ('website_published', '=', True)
+            ], limit=6)
 
         context = {
             'product': product,
@@ -68,6 +76,7 @@ class ShopController(WebsiteSale):
             'list_price': product.list_price,  # <-- Agrega esto
             'general_images': general_images,
             'brand_type_products_count': brand_type_products_count,
+            'related_tag_products': related_tag_products,
                  
         }
         return request.render("theme_xtream.website_view_product_xtream", context)
