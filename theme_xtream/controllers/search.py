8190b3e7b1074ea5a1b9c95beff7932f23350711
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-06-25 03:37:37 -0600

    Update search.py

--- theme_xtream/controllers/search.py
+++ theme_xtream/controllers/search.py
@@ -2,26 +2,20 @@ from odoo import http
 from odoo.http import request
 
 class WebsiteSearch(http.Controller):
-    @http.route('/search_redirect', auth='public', website=True)
-    def search_redirect(self, search='', search_type='all'):
-        Product = request.env['product.template'].sudo()
-        # Buscar por default_code exacto
-        product = Product.search([('default_code', '=', search)], limit=1)
-        if not product:
-            # Buscar por nombre exacto (case-insensitive)
-            product = Product.search([('name', 'ilike', search)], limit=1)
-        if product:
-            # Use the website URL or fallback to product.id if slug is not available
-            product_url = '/shop/product/%s' % product.id
-            return request.redirect('%s?product=product.template(%s,)' % (product_url, product.id))
 
+    @http.route('/search_redirect', auth='public', website=True)
+    def search_redirect(self, search='', search_type='all', **kw):
         if search_type == 'brand':
             return request.redirect('/brand_search_redirect?search=%s' % search)
         elif search_type == 'category':
             return request.redirect('/category_search?search=%s' % search)
         elif search_type == 'model':
-            # Ya se intentó buscar por default_code y nombre arriba
-            return request.redirect('/subcategory?search=%s' % search)
+            Product = request.env['product.template'].sudo()
+            product = Product.search([('default_code', '=', search)], limit=1)
+            if product:
+                return request.redirect('/shop/%s?product=product.template(%s,)' % (product.slug(), product.id))
+            else:
+                return request.redirect('/subcategory?search=%s' % search)
         else:
             # Buscar si el texto coincide con una marca activa
             Brand = request.env['brand.type'].sudo()
