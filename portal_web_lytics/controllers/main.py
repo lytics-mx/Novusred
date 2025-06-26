ffd4c1206bc11b9c39f16ca9a694604ed493bb7d
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-06-26 12:17:00 -0600

    remove_protal

--- portal_web_lytics/controllers/main.py
+++
@@ -1,7 +0,0 @@
-from odoo import http
-from odoo.http import request
-
-class MiSitioWeb(http.Controller):
-    @http.route('/lytics', type='http', auth='public', website=True)
-    def mi_pagina(self, **kw):
-        return request.render('portal_web_lytics.lytics_template', {})
