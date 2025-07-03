da92e7308a641643f24ab9ce56d2764390a9d713
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-07-02 21:23:32 -0600

    q

--- theme_xtream/controllers/shop.py
+++ theme_xtream/controllers/shop.py
@@ -8,9 +8,15 @@ class ShopController(WebsiteSale):
     def cart(self, **post):
         buy_now = post.get('buy_now') or request.httprequest.args.get('buy_now')
         if buy_now:
-            # Renderiza tu nueva plantilla personalizada
             return request.render('theme_xtream.website_cart_buy_now', self._prepare_cart_values())
-        # Si no es compra directa, usa el comportamiento normal
         return super().cart(**post)
-    
-    
+
+    @http.route('/shop/cart/remove', type='http', auth="public", website=True)
+    def cart_remove(self, line_id=None, **kw):
+        if line_id:
+            order = request.website.sale_get_order()
+            if order:
+                line = order.order_line.filtered(lambda l: l.id == int(line_id))
+                if line:
+                    line.unlink()
+        return request.redirect('/shop/cart')
