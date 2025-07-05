8c6f7362e3c33fb2ee1b5151c37091aa2b7ab412
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-07-05 04:53:46 -0600

    q

--- theme_xtream/controllers/wishlist.py
+++ theme_xtream/controllers/wishlist.py
@@ -4,12 +4,5 @@ from odoo.http import request
 class WishlistController(http.Controller):
     @http.route('/shop/wishlist', type='http', auth='public', website=True)
     def wishlist_page(self):
-        # Obtener los productos del wishlist del usuario actual
-        user_id = request.env.user.id
-        wishlist_items = request.env['product.wishlist'].sudo().search([('create_uid', '=', user_id)])
-        
-        # Preparar los productos para el template
-        products = wishlist_items.mapped('product_id')
-        
-        # Renderizar el template con los productos del wishlist
-        return request.render('theme_xtream.wishlist_template', {'products': products})
+        # Renderizar directamente el template sin lógica adicional
+        return request.render('theme_xtream.wishlist_template')
