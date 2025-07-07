8e2500fa7574b0926b4257d351a465af69fecf50
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-07-06 23:50:39 -0600

    update

--- theme_xtream/controllers/discover.py
+++ theme_xtream/controllers/discover.py
@@ -1,5 +1,6 @@
 from odoo import http
 from odoo.http import request
+import random
 
 class DiscoverController(http.Controller):
 
@@ -21,7 +22,16 @@ class DiscoverController(http.Controller):
                     'product_count': product_count
                 })
         
+        # Mezclar las categorías aleatoriamente
+        random.shuffle(category_data)
+        
+        # Dividir las categorías en tres columnas con un límite de 6 nombres por columna
+        columns = [category_data[i * 6:(i + 1) * 6] for i in range(3)]
+        
+        # Eliminar columnas vacías
+        columns = [column for column in columns if column]
+
         # Renderizar el template con los datos
         return request.render('theme_xtream.website_discover', {
-            'categories': category_data
+            'columns': columns
         })
