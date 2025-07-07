f3afe9d6e0be38ca5bf5b3634a2037b89997ab42
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-07-06 23:33:47 -0600

    Update discover.py

--- theme_xtream/controllers/discover.py
+++ theme_xtream/controllers/discover.py
@@ -12,12 +12,14 @@ class DiscoverController(http.Controller):
         category_data = []
         for category in categories:
             product_count = request.env['product.template'].sudo().search_count([
-                ('categ_id', 'child_of', category.id)
+                ('categ_id', 'child_of', category.id),
+                ('qty_available', '>', 1)  # Filtrar productos con stock mayor a 1
             ])
-            category_data.append({
-                'name': category.name,
-                'product_count': product_count
-            })
+            if product_count > 0:  # Solo incluir categorías con al menos un producto
+                category_data.append({
+                    'name': category.name,
+                    'product_count': product_count
+                })
         
         # Renderizar el template con los datos
         return request.render('theme_xtream.website_discover', {
