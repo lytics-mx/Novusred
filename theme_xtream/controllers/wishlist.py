0868a567beb8933176a609a061f4cdb2635a6a80
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-07-07 23:48:30 -0600

    Update wishlist.py

--- theme_xtream/controllers/wishlist.py
+++ theme_xtream/controllers/wishlist.py
@@ -1,5 +1,7 @@
 from odoo import http
 from odoo.http import request
+from odoo.http import JsonRequest
+
 
 class WishlistController(http.Controller):
     @http.route('/shop/wishlist', type='http', auth='public', website=True)
