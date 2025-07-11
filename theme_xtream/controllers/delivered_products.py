63dfe81b39a045f6022bc2dab444e070ab658f7c
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-07-11 01:28:07 -0600

    Update delivered_products.py

--- theme_xtream/controllers/delivered_products.py
+++ theme_xtream/controllers/delivered_products.py
@@ -15,7 +15,30 @@ class WebsiteCheckout(http.Controller):
             ('state', 'not in', ['done', 'cancel']),
             ('partner_id', '=', user.partner_id.id)
         ])
+        
+        # Obtener todos los productos de los pickings entregados
+        delivered_products = []
+        for picking in delivered_pickings:
+            for move in picking.move_ids_without_package:
+                delivered_products.append({
+                    'product_name': move.product_id.name,
+                    'quantity': move.product_qty,
+                    'delivery_date': picking.date_done,
+                    'image_url': f'/web/image/product.product/{move.product_id.id}/image_1920',
+                })
+        
+        # Obtener todos los productos de los pickings pendientes
+        pending_products = []
+        for picking in pending_pickings:
+            for move in picking.move_ids_without_package:
+                pending_products.append({
+                    'product_name': move.product_id.name,
+                    'quantity': move.product_qty,
+                    'scheduled_date': picking.scheduled_date,
+                    'image_url': f'/web/image/product.product/{move.product_id.id}/image_1920',
+                })
+        
         return request.render('theme_xtream.delivered_template', {
-            'delivered_pickings': delivered_pickings,
-            'pending_pickings': pending_pickings,
+            'delivered_products': delivered_products,
+            'pending_products': pending_products,
         })
