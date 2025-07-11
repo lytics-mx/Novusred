925d1943e40ff96d520c230d37091ac48f67985a
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-07-11 00:17:46 -0600

    update again sales

---
+++ sales_control/models/sales_order_line.py
@@ -0,0 +1,18 @@
+from odoo import models, fields, api
+
+class SaleOrderLine(models.Model):
+    
+    _inherit = 'sale.order.line'
+    @api.depends('product_id', 'product_uom_qty', 'discount', 'price_unit')
+    def _compute_amount(self):
+        for line in self:
+            # Verificar si el producto tiene la etiqueta específica
+            if line.product_id.tag_ids.filtered(lambda tag: tag.name == 'Discount'):
+                price_unit = line.product_id.discount_price
+            else:
+                price_unit = line.product_id.list_price
+
+            # Calcular el subtotal con el precio correcto
+            line.price_subtotal = price_unit * line.product_uom_qty * (1 - (line.discount or 0.0) / 100)
+            line.price_tax = line._compute_tax_amount()
+            line.price_total = line.price_subtotal + line.price_tax    
