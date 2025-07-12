d1d9bb20458626546b5d6e6a8c26701d41a23025
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-07-12 04:04:16 -0600

    update

--- theme_xtream/controllers/details_purchase.py
+++ theme_xtream/controllers/details_purchase.py
@@ -34,19 +34,19 @@ class ProductDetails(http.Controller):
         for picking in pickings:
             for move in picking.move_ids_without_package.filtered(lambda m: m.product_id.id == product_id):
                 state = picking.state or 'waiting'
-                state_index = tracking_states.index(state) if state in tracking_states else -1  # Validar estado
-
+                state_index = tracking_states.index(state) if state in tracking_states else -1
+        
                 purchase_details.append({
                     'quantity': move.product_qty,
                     'purchase_date': picking.date.strftime('%d de %B') if picking.date else '',
                     'delivery_date': picking.date_done,
-                    'state': state,  # Estado actual del picking
-                    'state_index': state_index,  # Índice del estado en tracking_states
-                    'tracking_states': tracking_states,  # Posibles estados
-                    'price': move.product_id.list_price,  # Precio del producto
-                    'total': move.product_qty * move.product_id.list_price,  # Total calculado
-                    'picking_origin': picking.origin,  # Identificador del picking (origin)
-                    'picking_name': picking.name,  # Nombre del picking
+                    'state': state,
+                    'state_index': state_index,
+                    'tracking_states': tracking_states,
+                    'price': move.product_id.list_price,
+                    'total': move.product_qty * move.product_id.list_price,
+                    'picking_origin': picking.origin,
+                    'picking_name': picking.name,
                 })
 
         return request.render('theme_xtream.product_details_template', {
