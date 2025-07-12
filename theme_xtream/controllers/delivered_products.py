49d263ccf9e7da71ade380d1cd0685858e3aa1ec
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-07-12 03:38:12 -0600

    update

--- theme_xtream/controllers/delivered_products.py
+++ theme_xtream/controllers/delivered_products.py
@@ -12,7 +12,7 @@ class WebsiteCheckout(http.Controller):
         ])
         # Filtrar pickings pendientes y relacionados con el usuario actual
         pending_pickings = request.env['stock.picking'].sudo().search([
-            ('state', 'not in', ['done', 'cancel']),
+            ('state', 'in', ['assigned', 'confirmed', 'waiting']),
             ('partner_id', '=', user.partner_id.id)
         ])
         
@@ -21,13 +21,11 @@ class WebsiteCheckout(http.Controller):
         for picking in delivered_pickings:
             for move in picking.move_ids_without_package:
                 delivered_products.append({
-                    'product_id': move.product_id.id,  # Agregar product_id
                     'product_name': move.product_id.name,
                     'quantity': move.product_qty,
                     'delivery_date': picking.date_done,
                     'purchase_date': picking.date.strftime('%d de %B') if picking.date else '',  # Formato de fecha
                     'image_url': f'/web/image/product.product/{move.product_id.id}/image_1920',
-                    'state': picking.state,  # Agregar el estado del stock.picking
                 })
         
         # Obtener todos los productos de los pickings pendientes
@@ -35,13 +33,11 @@ class WebsiteCheckout(http.Controller):
         for picking in pending_pickings:
             for move in picking.move_ids_without_package:
                 pending_products.append({
-                    'product_id': move.product_id.id,  # Agregar product_id
                     'product_name': move.product_id.name,
                     'quantity': move.product_qty,
                     'scheduled_date': picking.scheduled_date,
                     'purchase_date': picking.date.strftime('%d de %B') if picking.date else '',  # Formato de fecha
                     'image_url': f'/web/image/product.product/{move.product_id.id}/image_1920',
-                    'state': picking.state,  # Agregar el estado del stock.picking
                 })
         
         return request.render('theme_xtream.delivered_template', {
