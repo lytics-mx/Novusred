99bb9b1f6bd6cfacd12ecd3532a695a3a4e5ea3a
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-07-14 01:08:09 -0600

    update date

--- theme_xtream/controllers/delivered_products.py
+++ theme_xtream/controllers/delivered_products.py
@@ -1,89 +1,70 @@
 from odoo import http
 from odoo.http import request
 from babel.dates import format_date
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class WebsiteCheckout(http.Controller):
     @http.route(['/delivered_products'], type='http', auth='user', website=True)
     def delivered_products(self):
+        today = datetime.now().date()  # Fecha actual
         user = request.env.user
 
-        # Filtrar pickings completados y relacionados con el usuario actual
-        delivered_pickings = request.env['stock.picking'].sudo().search([
-            ('state', '=', 'done'),
+        # Filtrar movimientos relacionados con el usuario actual
+        stock_moves = request.env['stock.move'].sudo().search([
             ('partner_id', '=', user.partner_id.id)
         ])
-        # Filtrar pickings pendientes y relacionados con el usuario actual
-        pending_pickings = request.env['stock.picking'].sudo().search([
-            ('state', 'not in', ['done', 'cancel']),
-            ('partner_id', '=', user.partner_id.id)
-        ])
-        
-        # Obtener todos los productos de los pickings entregados
+
+        # Clasificar productos entregados y pendientes
         delivered_products = []
-        for picking in delivered_pickings:
-            for move in picking.move_ids_without_package:
-                date_deadline = picking.date_deadline
-                delivery_date = ''
-                if date_deadline:
-                    today = datetime.today()
-                    delta = (date_deadline - today).days
-                    if picking.state == 'done':
-                        delivery_date = f"Entregado el día {format_date(picking.date_done, format='d MMMM', locale='es')}" if picking.date_done else "Entregado"
-                    elif delta > 30:
-                        delivery_date = format_date(date_deadline, format="d 'de' MMMM yyyy", locale='es')
-                    elif delta > 7:
-                        delivery_date = format_date(date_deadline, format="d 'de' MMMM", locale='es')
-                    elif delta > 1:
-                        delivery_date = f"Llega en {delta} días"
-                    elif delta == 1:
-                        delivery_date = "Llega mañana"
-                    elif delta == 0:
-                        delivery_date = "Llega hoy"
-                    else:
-                        delivery_date = "Fecha límite pasada"
+        pending_products = []
 
-                purchase_date = ''
-                if picking.date:
-                    purchase_date = format_date(picking.date, format='d \'de\' MMMM', locale='es')
+        for move in stock_moves:
+            deadline_date = move.date_deadline.date() if move.date_deadline else None
+            delivery_date = move.date.date() if move.date else None
 
+            # Determinar el estado del producto
+            if move.state == 'done':  # Entregado
+                relative_date = format_date(delivery_date, format='d \'de\' MMMM', locale='es') if delivery_date else ''
                 delivered_products.append({
                     'product_id': move.product_id.id,
                     'product_name': move.product_id.name,
                     'quantity': move.product_qty,
-                    'delivery_date': delivery_date,  # Asegurarse de que siempre esté presente
-                    'purchase_date': purchase_date,
+                    'delivery_date': relative_date,
                     'image_url': f'/web/image/product.product/{move.product_id.id}/image_1920',
-                    'state': picking.state,
-                    'picking_origin': picking.origin,
-                    'picking_name': picking.name,
+                    'state': move.state,
+                    'picking_origin': move.picking_id.origin,
+                    'picking_name': move.picking_id.name,
                 })
-        
-        # Obtener todos los productos de los pickings pendientes
-        pending_products = []
-        for picking in pending_pickings:
-            for move in picking.move_ids_without_package:
-                scheduled_date = ''
-                if picking.scheduled_date:
-                    scheduled_date = format_date(picking.scheduled_date, format='d \'de\' MMMM', locale='es')
-
-                purchase_date = ''
-                if picking.date:
-                    purchase_date = format_date(picking.date, format='d \'de\' MMMM', locale='es')
+            else:  # Pendiente
+                if deadline_date:
+                    # Calcular la fecha relativa
+                    days_diff = (deadline_date - today).days
+                    if days_diff > 30:
+                        relative_date = format_date(deadline_date, format='d \'de\' MMMM', locale='es')
+                    elif days_diff > 7:
+                        relative_date = 'dentro de una semana'
+                    elif days_diff == 1:
+                        relative_date = 'mañana'
+                    elif days_diff == 0:
+                        relative_date = 'hoy'
+                    elif days_diff < 0:
+                        relative_date = 'atrasado'
+                    else:
+                        relative_date = f'en {days_diff} días'
+                else:
+                    relative_date = 'Sin fecha límite'
 
                 pending_products.append({
                     'product_id': move.product_id.id,
                     'product_name': move.product_id.name,
                     'quantity': move.product_qty,
-                    'scheduled_date': scheduled_date,
-                    'purchase_date': purchase_date,
-                    'delivery_date': '',  # Asegurarse de que siempre esté presente
+                    'deadline_date': relative_date,
                     'image_url': f'/web/image/product.product/{move.product_id.id}/image_1920',
-                    'state': picking.state,
-                    'picking_origin': picking.origin,
-                    'picking_name': picking.name,
+                    'state': move.state,
+                    'picking_origin': move.picking_id.origin,
+                    'picking_name': move.picking_id.name,
                 })
-        
+
         return request.render('theme_xtream.delivered_template', {
             'delivered_products': delivered_products,
             'pending_products': pending_products,
