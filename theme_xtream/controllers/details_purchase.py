8e9459815f5f5925784f2464c7d54ed640c5a195
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-07-13 22:24:12 -0600

    UPDATE

--- theme_xtream/controllers/details_purchase.py
+++ theme_xtream/controllers/details_purchase.py
@@ -38,6 +38,7 @@ class ProductDetails(http.Controller):
             'brand': brand_name,
             'brand_image_url': brand_image_url,
             'image_url': f'/web/image/product.product/{product.id}/image_1920',
+            'free_shipping': product.product_tmpl_id.free_shipping,  # Agregar el atributo free_shipping
         }
 
         # Preparar detalles de la compra y seguimiento
