91707ff8c20be80b247feebcf98d551730478b0f
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-07-21 04:00:56 -0600

    UPDATE

--- theme_xtream/controllers/product_views.py
+++ theme_xtream/controllers/product_views.py
@@ -9,7 +9,7 @@ import re
 _logger = logging.getLogger(__name__)
 
 class ShopController(WebsiteSale):
-
+    
     @http.route([
             '/shop/<string:product_slug>'
         ], type='http', auth="public", website=True, sitemap=False)
