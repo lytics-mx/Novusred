33faa76b0f6b95dc419c25cc2b1d4eebc9ac3de5
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-07-22 13:42:57 -0600

    Update sale_order_line.py

---
+++ inventory_control/models/sale_order_line.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    product_model = fields.Char(
+        string="Modelo",
+        related='product_id.product_tmpl_id.product_model',
+        store=True,
+        readonly=True
+    )
