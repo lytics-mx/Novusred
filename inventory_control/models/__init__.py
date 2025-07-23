603f71b0724d0846a04d28edae5944c4607eb834
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-07-23 02:36:05 -0600

    update new

--- inventory_control/models/__init__.py
+++ inventory_control/models/__init__.py
@@ -3,4 +3,8 @@ from . import brand_type
 from . import product_template
 from . import product_tag
 from . import product_image
+
+#Cotizaciones del modulo de CRM
 from . import product_product
+from . import sale_order_line
+from . import sale_order
