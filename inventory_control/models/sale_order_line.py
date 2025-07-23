b39ae91dd1737e0862264e59408959c141176cb5
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-07-22 23:00:57 -0600

    Update sale_order_line.py

--- inventory_control/models/sale_order_line.py
+++ inventory_control/models/sale_order_line.py
@@ -14,7 +14,7 @@ class SaleOrderLine(models.Model):
     def _onchange_product_id(self):
         for line in self:
             if line.product_id:
-                # Asignar solo el código interno (default_code) o el nombre corto si no hay código
-                line.name = line.product_id.default_code or line.product_id.name
+                # Solo asignar el nombre sin código
+                line.name = line.product_id.name
             else:
                 line.name = ''
