08615a6db480ca3e2325bccfba86b3299809a3c2
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-07-23 03:43:49 -0600

    a

--- inventory_control/models/product_product.py
+++ inventory_control/models/product_product.py
@@ -32,10 +32,10 @@ class ProductProduct(models.Model):
 
     def name_get(self):
         result = []
-        for product in self:
+        for template in self:
             # Mostrar primero el modelo y luego el nombre del producto
-            name = f"{product.product_model or ''} - {product.name or ''}"
-            result.append((product.id, name))
+            name = f"{template.product_model or ''} - {template.name or ''}"
+            result.append((template.id, name))
         return result
     
     @property
