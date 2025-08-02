17b424add57d310c66e75192361ed86dafe378d2
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-08-02 00:50:17 -0600

    Update search.py

--- theme_xtream/controllers/search.py
+++ theme_xtream/controllers/search.py
@@ -23,7 +23,7 @@ class WebsiteSearch(http.Controller):
 
         # Si se encuentra un producto, redirigir a su página usando website_url
         if product and product.website_url:
-            return request.redirect(product.website_url)
+            return request.redirect(product.website_url + f"?product=product.template({product.id},)")
 
         # Si no se encuentra ningún producto, redirigir a la búsqueda general
         if search_type == 'brand':
