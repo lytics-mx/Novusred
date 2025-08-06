ca780c259471233c1b8671b540f7835f655bef86
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-08-06 13:21:04 -0600

    Update sale_order.py

--- inventory_control/models/sale_order.py
+++ inventory_control/models/sale_order.py
@@ -3,6 +3,31 @@ from odoo import models, api, fields
 class UpdateSaleOrderSequence(models.Model):
     _inherit = 'sale.order'
 
+    # Sobrescribir el campo 'note' para que siempre tenga el valor "Ejecutivo de cuenta"
+    note = fields.Text(
+        string="Ejecutivo de cuenta",  # Cambiar la etiqueta del campo a "Ejecutivo de cuenta"
+        default="Ejecutivo de cuenta",  # Establecer un valor predeterminado
+        required=False
+    )
+
+    @api.model
+    def create(self, vals):
+        # Forzar el valor de 'note' al crear un nuevo registro
+        vals['note'] = "Ejecutivo de cuenta"
+        return super().create(vals)
+
+    def write(self, vals):
+        # Forzar el valor de 'note' al actualizar un registro
+        vals['note'] = "Ejecutivo de cuenta"
+        return super().write(vals)
+
+    @api.model
+    def update_existing_notes(self):
+        # Actualizar todos los registros existentes para que el campo 'note' sea "Ejecutivo de cuenta"
+        orders = self.search([])
+        orders.write({'note': "Ejecutivo de cuenta"})
+
+
     @api.model
     def update_sale_order_sequence(self):
         # Buscar la secuencia de las órdenes de venta
