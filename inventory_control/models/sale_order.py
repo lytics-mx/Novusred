2dbeff535c7e57bdc05bf91d8ae924a17ba3f212
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-08-06 12:56:05 -0600

    Update sale_order.py

--- inventory_control/models/sale_order.py
+++ inventory_control/models/sale_order.py
@@ -2,7 +2,13 @@ from odoo import models, api, fields
 
 class UpdateSaleOrderSequence(models.Model):
     _inherit = 'sale.order'
-
+    # Cambiar el nombre del campo "Vendedor" a "Ejecutivo de cuenta"
+    user_id = fields.Many2one(
+        'res.users',
+        string="Ejecutivo de cuenta",
+        tracking=True,
+        default=lambda self: self.env.user
+    )
     @api.model
     def update_sale_order_sequence(self):
         # Buscar la secuencia de las órdenes de venta
