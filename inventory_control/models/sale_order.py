9b7009810e1d2563d847a020f0af3af308b49468
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-08-06 12:24:01 -0600

    Update sale_order.py

--- inventory_control/models/sale_order.py
+++ inventory_control/models/sale_order.py
@@ -4,23 +4,6 @@ class UpdateSaleOrderSequence(models.Model):
     _inherit = 'sale.order'
 
     @api.model
-    def create(self, vals):
-        # Forzar el valor de 'note' al crear un nuevo registro
-        vals['note'] = "Términos y Condiciones en: https://novusred.mx/content/2-Terminos-y-Condiciones"
-        return super().create(vals)
-
-    def write(self, vals):
-        # Forzar el valor de 'note' al actualizar un registro
-        vals['note'] = "Términos y Condiciones en: https://novusred.mx/content/2-Terminos-y-Condiciones"
-        return super().write(vals)
-
-    @api.model
-    def force_update_existing_notes(self):
-        # Actualizar forzosamente el campo 'note' en todos los registros existentes
-        orders = self.search([])
-        orders.write({'note': "Términos y Condiciones en: https://novusred.mx/content/2-Terminos-y-Condiciones"})
-    
-    @api.model
     def update_sale_order_sequence(self):
         # Buscar la secuencia de las órdenes de venta
         sequence = self.env['ir.sequence'].search([('code', '=', 'sale.order')], limit=1)
