9c361803d136cee4d88778b45d33583fa34f29be
jennifer015 <151900336+jennifer015@users.noreply.github.com>
2025-10-01 22:18:55 -0600

    Update sale_order_line.py

--- inventory_control/models/sale_order_line.py
+++ inventory_control/models/sale_order_line.py
@@ -20,20 +20,3 @@ class SaleOrderLine(models.Model):
             result.append((product.id, name))
         return result
             
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            display_type = vals.get('display_type')
-            # Solo validar si NO es nota ni sección
-            if display_type not in ('line_section', 'line_note') and not vals.get('product_id'):
-                raise ValidationError(_("Debe seleccionar un producto para cada línea de pedido."))
-        return super().create(vals_list)
-    
-    def write(self, vals):
-        for line in self:
-            display_type = vals.get('display_type', line.display_type)
-            product_id = vals.get('product_id', line.product_id.id)
-            # Solo validar si NO es nota ni sección
-            if display_type not in ('line_section', 'line_note') and not product_id:
-                raise ValidationError(_("Debe seleccionar un producto para cada línea de pedido."))
-        return super().write(vals)
