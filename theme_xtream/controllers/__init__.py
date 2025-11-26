c40a59adb0f4f97984d3ec0e7aa25e20a81213d2
Fernando Paz <151900336+jennifer015@users.noreply.github.com>
2025-11-25 23:17:52 -0600

    a

--- theme_xtream/controllers/__init__.py
+++ theme_xtream/controllers/__init__.py
@@ -5,15 +5,11 @@ from . import historial_views
 from . import category_views
 from . import brand_views
 
-#from . import search
-
-# from . import product_views
-# from . import img_banner
 
 from . import search
 from . import cart
 from . import login
-from . import wishlist
+# from . import wishlist
 from . import discover
 from . import checkout
 from . import delivered_products
