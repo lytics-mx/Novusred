463a16d8df7d1ee49f99c82861eff57df9485a27
Fernando Paz <151900336+jennifer015@users.noreply.github.com>
2025-11-26 00:19:57 -0600

    Update wishlist.py

--- theme_xtream/controllers/wishlist.py
+++ theme_xtream/controllers/wishlist.py
@@ -2,7 +2,7 @@ from odoo import http
 from odoo.http import request
 
 class WishlistController(http.Controller):
-    @http.route('/shop/wishlist', type='http', auth='public', website=True)
+    @http.route('/wishlist', type='http', auth='public', website=True)
     def wishlist_page(self):
         return request.render('theme_xtream.wishlist_template')
 
