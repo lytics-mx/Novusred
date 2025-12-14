22198fb380e83287e37d23ae5c4506d80945cb1d
Fernando Paz <151900336+jennifer015@users.noreply.github.com>
2025-12-14 04:07:27 -0600

    a

--- theme_xtream/__manifest__.py
+++ theme_xtream/__manifest__.py
@@ -11,66 +11,66 @@
     'website': "https://www.cybrosys.com",
     'depends': ['product', 'stock', 'base','inventory_control', 'website','website_sale', 'website_sale_wishlist', 'website_mass_mailing'],
     'data': [
-        'security/ir.model.access.csv',
-        'views/xtream_testimonials_views.xml',
-        'views/contact_us_templates.xml',
-        'views/footer_templates.xml',
-        'views/shop_templates.xml',
-        'views/header_templates.xml',
-        'views/snippets/snippets_templates.xml',
-        'views/snippets/amazing.xml',
-        'views/snippets/new_arrivals.xml',
-        'views/snippets/discount.xml',
-        'views/snippets/main_banner.xml',
-        'views/snippets/main_product.xml',
-        'views/snippets/testimonial.xml',
+        # 'security/ir.model.access.csv',
+        # 'views/xtream_testimonials_views.xml',
+        # 'views/contact_us_templates.xml',
+        # 'views/footer_templates.xml',
+        # 'views/shop_templates.xml',
+        # 'views/header_templates.xml',
+        # 'views/snippets/snippets_templates.xml',
+        # 'views/snippets/amazing.xml',
+        # 'views/snippets/new_arrivals.xml',
+        # 'views/snippets/discount.xml',
+        # 'views/snippets/main_banner.xml',
+        # 'views/snippets/main_product.xml',
+        # 'views/snippets/testimonial.xml',
 
 # INFORMACIÓN DE POLITICAS        
-        'views/policies/about_us_templates.xml',
-        'views/policies/terms_and_conditions.xml',
-        'views/policies/purchasing_policies.xml',
-        'views/policies/warranty_policies.xml',
-        'views/policies/delivery_policies.xml',
-        'views/policies/privacy_policy.xml',
-        'views/policies/refund_policies.xml',
-        'views/policies/payment_policies.xml',
+        # 'views/policies/about_us_templates.xml',
+        # 'views/policies/terms_and_conditions.xml',
+        # 'views/policies/purchasing_policies.xml',
+        # 'views/policies/warranty_policies.xml',
+        # 'views/policies/delivery_policies.xml',
+        # 'views/policies/privacy_policy.xml',
+        # 'views/policies/refund_policies.xml',
+        # 'views/policies/payment_policies.xml',
 
 
 
 # Configuiración del modulo de sitio web en odoo
-        'views/website_configuration/imagenes_banner.xml',
+        # 'views/website_configuration/imagenes_banner.xml',
         # 'views/website_configuration/free_shipping.xml',
 
 # Vistas de la pagina web
-        'views/website_sale/website_history.xml',
-        'views/website_sale/website_category.xml',
-        'views/website_sale/website_subcategory.xml',
-        'views/website_sale/website_brand.xml',
-        'views/website_sale/website_view_products.xml',
-        'views/website_sale/website_offers.xml',
-        'views/website_sale/website_home.xml',
-        'views/website_sale/website_login.xml',
-        'views/website_sale/website_user_record.xml',
-        'views/website_sale/website_whislist.xml',
-        'views/website_sale/website_discover.xml',
-        'views/website_sale/website_signup_success.xml',
+        # 'views/website_sale/website_history.xml',
+        # 'views/website_sale/website_category.xml',
+        # 'views/website_sale/website_subcategory.xml',
+        # 'views/website_sale/website_brand.xml',
+        # 'views/website_sale/website_view_products.xml',
+        # 'views/website_sale/website_offers.xml',
+        # 'views/website_sale/website_home.xml',
+        # 'views/website_sale/website_login.xml',
+        # 'views/website_sale/website_user_record.xml',
+        # 'views/website_sale/website_whislist.xml',
+        # 'views/website_sale/website_discover.xml',
+        # 'views/website_sale/website_signup_success.xml',
 
 
 
 
 # Búsqueda de productos en la web (incluye marcas y categorías)
-        'views/website_sale/search/brand_search.xml',
-        'views/website_sale/search/category_search.xml',
-        'views/website_sale/search/nav_bar.xml',
-        'views/website_sale/search/notificacion.xml',
+        # 'views/website_sale/search/brand_search.xml',
+        # 'views/website_sale/search/category_search.xml',
+        # 'views/website_sale/search/nav_bar.xml',
+        # 'views/website_sale/search/notificacion.xml',
         
 
 
-        'views/shop/website_cart.xml',
-        'views/shop/website_purchase.xml',
-        'views/shop/website_details_purchase.xml',
+        # 'views/shop/website_cart.xml',
+        # 'views/shop/website_purchase.xml',
+        # 'views/shop/website_details_purchase.xml',
 
-        'views/csrf_token.xml',
+        # 'views/csrf_token.xml',
 
 # Vistas para móviles        
         # 'views/mobil/mobile_viewproduct.xml',
